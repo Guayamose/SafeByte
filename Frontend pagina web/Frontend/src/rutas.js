764135bec759bc0aa6152f266955637e7b4b39cd
Guayamose <tasfa66@gmail.com>
2025-02-04 18:44:14 +0100

    errores en las rutas de las ventanas, el index.html no detecta las rutas de javascript y el icono de la web no aparece en el buscador

--- Frontend pagina web/Frontend/src/rutas.js
+++
@@ -1,6 +0,0 @@
-export const rutas = {
-    home: 'src/ventanas/Home.js',
-    login: 'src/ventanas/VentanaLogin.js',
-    registro: 'src/ventanas/VentanaRegistro.js',
-    comidas: 'src/ventanas/VentanaComida.js'
-};
