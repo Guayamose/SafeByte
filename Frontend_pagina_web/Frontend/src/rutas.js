e787f732599119f06bb81b0be0cb105d1a5935cb
Rafa Pradillo <63518682+Guayamose@users.noreply.github.com>
2025-02-04 19:05:56 +0100

    Delete Frontend_pagina_web/Frontend directory

--- Frontend_pagina_web/Frontend/src/rutas.js
+++
@@ -1,7 +0,0 @@
-export const rutas = {
-    home: './ventanas/Home.js',
-    login: './ventanas/VentanaLogin.js',
-    registro: './ventanas/VentanaRegistro.js',
-    comidas: './ventanas/VentanaComida.js'
-};
-
--- Frontend_pagina_web/Frontend/src/ventanas/Home.js
+++
@@ -1,7 +0,0 @@
-export default function mostrarHome() {
-    const main = document.getElementById('main-content');
-    main.innerHTML = `
-        <h2>Bienvenido a Food DNA</h2>
-        <p>Selecciona una opción en el menú para comenzar.</p>
-    `;
-}
