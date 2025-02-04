44b11e49f45792804d6877568226c665c8f845d5
Guayamose <tasfa66@gmail.com>
2025-02-04 17:27:15 +0100

    Update estructura

---
+++ Frontend pagina web/Frontend/src/rutas.js
@@ -0,0 +1,6 @@
+export const rutas = {
+    home: 'ventanas/Home.js',
+    login: 'ventanas/VentanaLogin.js',
+    registro: 'ventanas/VentanaRegistro.js',
+    comidas: 'ventanas/VentanaComida.js'
+};
