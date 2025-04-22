08c07d00354193fcae2ec85ec664d80cc6b7a2c5
gnzl00 <9200492@alumnos.ufv.es>
2025-04-22 11:14:36 +0200

    Escaner?
    No sabemos si funciona, depende del ordenador, la luz y lo que dios quiera

--- Backend1/obj/Debug/net8.0/Backend1.AssemblyInfo.cs
+++ Backend1/obj/Debug/net8.0/Backend1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3eb9826e627353fcff044c6194dc3d7a4d5aaca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32216779097f9c5213ffc1c6f2c5da66d311f0bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
